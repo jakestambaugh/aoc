63571b9f7748680c701f690b14259f9457104293
Jake Stambaugh <jake.stambaugh@gmail.com>
2023-12-11 02:28:42 +0000

    working on a vector of pointers for generic use

--- 2023/day00/solution.c
+++ 2023/day00/solution.c
@@ -2,10 +2,142 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <string.h>
+
+typedef struct
+{
+  size_t *data;
+  size_t *tail;
+  long size;
+  long capacity;
+} vector;
+
+vector *vector_new()
+{
+  vector *v = (vector *)malloc(sizeof(vector));
+
+  // Start with 16 elements by default
+  long starting_capacity = 16;
+  char *data = (char *)malloc(sizeof(size_t) * starting_capacity);
+  if (data == NULL || v == NULL)
+  {
+    printf("Failed to allocate memory for vector\n");
+    return NULL;
+  }
+
+  v->data = data;
+  v->tail = v->data;
+
+  v->size = 0;
+  v->capacity = starting_capacity;
+  return v;
+}
+
+void vector_free(vector *v)
+{
+  free(v->data);
+  free(v);
+}
+
+vector *vector_resize(vector *v)
+{
+  // always double
+  long new_capacity = v->capacity * 2;
+  size_t *new_buffer = (size_t *)realloc((void *)v->data, new_capacity);
+  if (new_buffer == NULL)
+  {
+    fprintf(stderr, "Failed to reallocated memory for vector expansion\n");
+    return NULL;
+  }
+  v->data = new_buffer;
+  v->tail = v->data + (size_t)(sizeof(size_t) * v->size);
+  v->capacity = new_capacity;
+}
+
+void vector_push(vector *v, char value)
+{
+  if (v->size + 1 >= v->capacity)
+  {
+    vector *new = vector_resize(v);
+    if (new == NULL)
+    {
+      fprintf(stderr, "Failed to push element to vector\n");
+      return;
+    }
+  }
+}
+
+/**
+ * Read the content of a file into a buffer.
+ *
+ * This function dynamically allocates memory for the buffer.
+ * Remember to free it.
+ */
+char *read_file_into_memory(const char *filename)
+{
+  FILE *file = fopen(filename, "r");
+  if (file == NULL)
+  {
+    printf("Failed to open file %s\n", filename);
+    return NULL;
+  }
+
+  // Get the file size
+  fseek(file, 0, SEEK_END);
+  long file_size = ftell(file);
+  rewind(file);
+
+  // Allocate memory for the file content
+  char *buffer = (char *)malloc(file_size + 1);
+  if (buffer == NULL)
+  {
+    printf("Failed to allocate memory\n");
+    fclose(file);
+    return NULL;
+  }
+
+  // Read the file into the buffer
+  size_t result = fread(buffer, 1, file_size, file);
+  if (result != file_size)
+  {
+    printf("Failed to read file\n");
+    free(buffer);
+    fclose(file);
+    return NULL;
+  }
+
+  // Null-terminate the buffer
+  buffer[file_size] = '\0';
+
+  fclose(file);
+  return buffer;
+}
+
+vector *split_lines(char *buffer)
+{
+  vector *v = vector_new();
+  char *current = buffer;
+  size_t while (*current != '\0')
+  {
+    if (*current == '\n')
+    {
+      *current = '\0';
+    }
+    current++;
+  }
+  return buffer;
+}
 
 void part1(char *filename)
 {
   printf("Part 1: %s\n", filename);
+  char *buffer = read_file_into_memory(filename);
+  char *lines = split_lines(buffer);
+  while (*lines != '\0')
+  {
+    printf("%s\n", lines);
+    lines += strlen(lines) + 1;
+  }
 }
 
 void part2(char *filename)
@@ -26,16 +158,12 @@ int main(int argc, char *argv[])
       {NULL, 0, NULL, 0}};
   while ((opt = getopt_long(argc, argv, "p:t:", longopts, &indexptr)) != -1)
   {
-    // printf("opt: %c\n", (char)opt);
     switch (opt)
     {
     case 'p':
-      // printf("Option p with argument %s\n", optarg);
-      // printf("part: %d\n", part);
       part = atoi(optarg);
       break;
     case 't':
-      // printf("Option t with argument %s\n", optarg);
       filename = "test.txt";
       break;
     default:
@@ -43,16 +171,13 @@ int main(int argc, char *argv[])
       return 1;
     }
   }
-  // printf("Filename: %s\n", filename);
 
   if (part == 1)
   {
-    // printf("Part 1\n");
     part1(filename);
   }
   else if (part == 2)
   {
-    // printf("Part 2\n");
     part2(filename);
   }
   else
