7c8c53224fdca6aa265a71160adc205689abddd4
Jake Stambaugh <jake.stambaugh@gmail.com>
2023-12-18 03:10:23 +0000

    Finish day 2 in python

--- 2023/day02/solution.py
+++ 2023/day02/solution.py
@@ -10,11 +10,47 @@ def get_lines(filename):
         return contents.strip().split("\n")
 
 
+def min_cubes(line):
+    parts = line.split(":")
+    game, rest = parts[0], parts[1]
+    pulls = rest.split(";")
+    r, g, b = 0, 0, 0
+    for pull in pulls:
+        colors = pull.split(",")
+        for color in colors:
+            halves = color.split(" ")
+            if halves[2] == "red" and r < int(halves[1]):
+                r = int(halves[1])
+            elif halves[2] == "green" and g < int(halves[1]):
+                g = int(halves[1])
+            elif halves[2] == "blue" and b < int(halves[1]):
+                b = int(halves[1])
+    return (r, g, b)
+
+
+def valid_color(count, color):
+    if color == "red" and count > 12:
+        return False
+    elif color == "green" and count > 13:
+        return False
+    elif color == "blue" and count > 14:
+        return False
+    else:
+        return True
+
+
 def possible_game(line):
-    game, rest = line.split(":")
-    print(game)
-    print(rest)
-    return 0
+    parts = line.split(":")
+    game, rest = parts[0], parts[1]
+    game_num = game.split(" ")[1]
+    pulls = rest.split(";")
+    possible = True
+    for pull in pulls:
+        colors = pull.split(",")
+        for color in colors:
+            halves = color.split(" ")
+            possible = possible and valid_color(int(halves[1]), halves[2])
+    return int(game_num) if possible else 0
 
 
 def part1(filename):
@@ -27,7 +63,13 @@ def part1(filename):
 
 
 def part2(filename):
-    print(f"Part 2: {filename}")
+    lines = get_lines(filename)
+    total = 0
+    for line in lines:
+        cubes = min_cubes(line)
+        power = cubes[0] * cubes[1] * cubes[2]
+        total += power
+    print(f"Part 2: {total}")
 
 
 if __name__ == "__main__":
