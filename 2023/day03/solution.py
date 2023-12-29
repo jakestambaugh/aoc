c86955fc63cef538abbd8a022b824b735a4e9365
Jake Stambaugh <jake.stambaugh@gmail.com>
2023-12-29 18:24:31 +0000

    Finished Day 03 p2 in Python

--- 2023/day03/solution.py
+++ 2023/day03/solution.py
@@ -2,6 +2,11 @@
 Advent of Code utilities and skeleton
 """
 import argparse
+from collections import defaultdict
+
+
+def flatten_list(xss):
+    return [x for xs in xss for x in xs]
 
 
 def get_lines(filename):
@@ -87,25 +92,43 @@ def flood_number(grid, i, j):
 
 
 def find_symbols(grid):
-    symbol_positions = []
+    symbol_positions = defaultdict(list)
     for i, row in enumerate(grid):
         for j, item in enumerate(row):
             if not item.isdigit() and item != ".":
-                symbol_positions.append((i, j))
+                symbol_positions[item].append((i, j))
     return symbol_positions
+
+
+def calculate_ratios(grid, gears):
+    ratios = []
+    for gear in gears:
+        detected_numbers = flood_number(grid, *gear)
+        if len(detected_numbers) == 2:
+            ratio = detected_numbers[0] * detected_numbers[1]
+            ratios.append(ratio)
+    return ratios
 
 
 def part1(filename):
     lines = get_lines(filename)
     grid = build_grid(lines)
     symbol_positions = find_symbols(grid)
-    detected_numbers = [flood_number(grid, *pos) for pos in symbol_positions]
-    total = sum(sum(n) for n in detected_numbers)
+    detected_numbers = [
+        flood_number(grid, *pos) for pos in flatten_list(symbol_positions.values())
+    ]
+    total = sum(flatten_list(detected_numbers))
     print(f"Part 1: {total}")
 
 
 def part2(filename):
-    print(f"Part 2: {filename}")
+    lines = get_lines(filename)
+    grid = build_grid(lines)
+    symbol_positions = find_symbols(grid)
+    gears = symbol_positions["*"]
+    ratios = calculate_ratios(grid, gears)
+    total = sum(ratios)
+    print(f"Part 2: {total}")
 
 
 if __name__ == "__main__":
